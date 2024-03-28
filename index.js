c214ff6915ec3dd1cbda6eda691032e978e55508
KRITHIKus <krithikus2004@gmail.com>
2024-03-28 13:02:57 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+var sideNavbar = document.querySelector('.side-navbar');
+function shownavbar() {
+    
+    sideNavbar.style.left= '0'; // or any other style you want to set
+}
+function cloasenavbar(){
+   
+    sideNavbar.style.left = '-60%';
+}
